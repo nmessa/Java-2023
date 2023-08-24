9abbf81399588bd7f3c72bd6f3b734ce5dbd2e55
nmessa <nmessa@sau16.org>
2023-08-24 09:57:07 -0400

    Add files via upload

---
+++ Lab Exercise 8.30.2023/NameThatCelebrity/src/NameThatCelebrity.java
@@ -0,0 +1,27 @@
+/**
+ * @(#)NameThatCelebrity.java
+ *
+ * NameThatCelebrity application
+ *
+ * @Author: 
+ * @version 1.00 2023/8/30
+ */
+ 
+public class NameThatCelebrity {
+    
+    public static void main(String[] args) {
+    	// Declare variables
+    	String s1 = "Allan Alda";
+    	String s2 = "John Wayne";
+    	String s3 = "Gregory Peck";
+    	int len;
+    	
+    	//Add code here
+    	
+    }
+}
+
+//Output
+//lan A
+//hn Wa
+//egory P
