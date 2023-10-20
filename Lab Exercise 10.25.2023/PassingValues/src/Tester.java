c58306e5b17203e9f84a913e088bd807b6fb7381
nmessa <nmessa@sau16.org>
2023-10-20 07:22:23 -0400

    Add files via upload

---
+++ Lab Exercise 10.25.2023/PassingValues/src/Tester.java
@@ -0,0 +1,19 @@
+//Lab Exercise 10/25/2023
+//Author: nmessa
+
+class Tester {
+	public static void main(String args[ ])
+	{
+		//Add code here
+		
+	}
+
+
+	public static void myMethod(double stuff, MyClass anObj, int a[ ], String s)
+	{
+		//Add code here
+		
+		
+	}
+}
+
